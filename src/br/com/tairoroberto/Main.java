c9a36728ef4c36b1a98ac6c913a3e5939ae0ddcc
tairo.toberto <tairoroberto@hotmail.com>
2022-01-20 10:46:11 -0300

    Adicionando log do git

--- src/br/com/tairoroberto/Main.java
+++ src/br/com/tairoroberto/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World -> Tairo");
+        System.out.println("Printand  com test do github");
     }
 }
